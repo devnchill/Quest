73944cb3698292b2cfdf3ee535161dced6e063b1
0xDev <169875752+devnchill@users.noreply.github.com>
2026-01-02 22:26:12 +0530

    spent almost an hr but worth it

---
+++ haskell/CIS-194/Week-01/towerOfHanoi.hs
@@ -0,0 +1,9 @@
+type Peg = String
+
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi x a b c
+  | x == 1 = [(a, b)]
+  | otherwise = hanoi (x - 1) a c b ++ [(a, b)] ++ hanoi (x - 1) c b a
+
