bb8e4ae527dd39e3f017004f6a2f387a09320f77
Viena <169875752+devnchill@users.noreply.github.com>
2026-02-18 17:49:09 +0530

    chore: devshell for rust

---
+++ Rust/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let 
+  	system  = "x86_64-linux";
+	pkgs = nixpkgs.legacyPackages.${system};
+   in 
+  {
+	devShells.${system}.default = pkgs.mkShell{
+		packages = with pkgs;[
+			nixfmt 
+			nil 
+			cargo
+			rustc
+			rust-analyzer
+			rustfmt
+			pkg-config
+			openssl
+		];
+		env = {
+			RUST_BACKTRACE = "full";
+		};
+	};	
+
+  };
+}
