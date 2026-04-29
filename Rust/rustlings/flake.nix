38b7a8dc02a9e224c3ffd3507532c44abc6e8616
Viena <169875752+devnchill@users.noreply.github.com>
2026-04-29 08:22:33 +0530

    chore: add rust overlay

--- Rust/rustlings/flake.nix
+++ Rust/rustlings/flake.nix
@@ -3,32 +3,35 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          nixfmt
-          nil
-          cargo
-          rustc
-          rust-analyzer
-          clippy
-          rustfmt
-          pkg-config
-          openssl
-          rustlings
-        ];
-        env = {
-          RUST_BACKTRACE = "full";
-        };
-      };
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              openssl
+              pkg-config
+              rust-bin.stable.latest.default
+            ];
+          };
 
-    };
+      }
+    );
 }
